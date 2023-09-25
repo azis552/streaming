79b62631bd6816f1c5221ce36283a249ef7d18d9
azis552 <azisaan552@gmail.com>
2023-09-25 11:55:09 +0700

    aplikasi streaming

---
+++ application/views/login/template/footer.php
@@ -0,0 +1,9 @@
+<!-- jQuery -->
+<script src="<?= base_url() ?>assets/login/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url() ?>assets/login/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url() ?>assets/login/dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
